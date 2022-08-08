4c38575f3fd1f67423fe7702d7b6562fd92fdee7
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-08 18:23:24 +0300

    added basic navbar and footer components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+
       hi
+      <Footer />
     </div>
   );
 }

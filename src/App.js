9179e4cdae89d32af95edba05ca5c493373329f3
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-09 18:02:58 +0300

    activated react router & styled home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import SignUp from './Components/SignUp';
+import SignIn from './Components/SignIn';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-
-      hi
+      <BrowserRouter >
+      <Route path="/home" component={Home} />
+      <Link>
+      <Route path="/signUp" component={SignUp} />
+      <Route exact path="/signIn" component={SignIn} />
+      </Link>
+      </BrowserRouter>
       <Footer />
     </div>
   );
---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='img'>
+      {/* <img src="../../public/imgs/homePageBG.avif" alt="HomePage" /> */}
+    </div>
+  )
+}
+
+export default Home

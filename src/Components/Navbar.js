4dd55aa8e0a6863517ba184ebf638fdb727e20c2
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-09 19:02:17 +0300

    added logo to navbar and created realistic nav items

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,17 +2,24 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>
+      <>
       <ul>
-      <li>Nav Item</li>
-      <li>Nav Item</li>
-      <li>Nav Item</li>
+      <li><a class="navbar-logo" href="/">
+      <div class="logo-image">
+      <img src="" class="img-fluid" alt=''/>
+      </div>
+      </a></li>
+      <div className='nav-items'>
+      <li>Our Services</li>
+      <li>Our Staff</li>
+      <li>Contact Us</li>
+      </div>
       <div className='btns-wrapper'>
       <button>Sign Up</button>
       <button>Sign In</button>
       </div>
       </ul>
-      </div>
+      </>
   )
 }
 

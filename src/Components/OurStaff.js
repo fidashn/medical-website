29f20a7702e520df652bcb12f52bd2b300ef5787
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-09 21:55:33 +0300

    created the staff page with images

--- src/Components/OurStaff.js
+++ src/Components/OurStaff.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 function OurStaff() {
   return (
-    <div className='gallery'><h2>OurStaff</h2>
-    
+    <div className='gallery'>
+    <h2>OurStaff</h2>
+    <div className='image1'><img src="../../public/imgs/doctor1.avif" alt="" /></div>
+    <div className='image2'><img src="../../public/imgs/doctor2.avif" alt="" /></div>
+    <div className='image3'><img src="../../public/imgs/doctor3.avif" alt="" /></div>
+    <div className='image4'><img src="../../public/imgs/doctor4.avif" alt="" /></div>
     
     </div>
   )

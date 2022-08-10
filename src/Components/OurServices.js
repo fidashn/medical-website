d7659fad3734d700ed8a2fd9a2fb002907f46036
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-10 18:32:12 +0300

    added images to services page

--- src/Components/OurServices.js
+++ src/Components/OurServices.js
@@ -7,9 +7,13 @@ function OurServices() {
     In our clinic you can:
     <ul className='services'>
     <li>check your blood presure levels</li>
+    <img src="" alt="heart-beat" className='heart-beat' />
     <li>check your sugar levels</li>
+    <img src="" alt="blood-test"  className='blood-test'/>
     <li>schedule an appointment online</li>
+    <img src="" alt="online-appointment"  className='online-appointment'/>
     <li>video chat with your doctor</li>
+    <img src="" alt="video-chat"  className='video-chat'/>
     </ul>
     </div>
   )

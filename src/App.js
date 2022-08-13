5a0e73d12dc235f55972d65ac12f6aa0e03d7461
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-14 01:07:36 +0300

    fixed the link bug

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import SignUp from './Components/SignUp';
 import SignIn from './Components/SignIn';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route} from "react-router-dom";
 import OurServices from './Components/OurServices';
 import OurStaff from './Components/OurStaff';
 import ContactUs from './Components/ContactUs';
@@ -16,13 +16,11 @@ function App() {
       <Navbar />
       <BrowserRouter >
       <Route path="/home" component={Home} />
-      <Link>
       <Route path="/signUp" component={SignUp} />
       <Route exact path="/signIn" component={SignIn} />
       <Route exact path="/ourServices" component={OurServices} />
       <Route exact path="/ourStaff" component={OurStaff} />
       <Route exact path="/contactUs" component={ContactUs} />
-      </Link>
       </BrowserRouter>
       <Footer />
     </div>

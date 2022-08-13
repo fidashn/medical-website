338431359c50341fe943085cac39ab0d72f949ca
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-14 00:48:26 +0300

    added paragraphs

--- src/Components/OurServices.js
+++ src/Components/OurServices.js
@@ -7,12 +7,16 @@ function OurServices() {
     In our clinic you can:
     <ul className='services'>
     <li>check your blood presure levels</li>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam recusandae vitae asperiores alias dolores temporibus ipsam perferendis id voluptatibus, fugiat sint ad expedita dolorum voluptatum ut deserunt explicabo voluptate nobis iure animi. Hic odio animi labore nostrum itaque nam repudiandae porro. Cumque beatae velit ullam aspernatur aliquam! Nobis, dolores alias?</p>
     <img src="" alt="heart-beat" className='heart-beat' />
     <li>check your sugar levels</li>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam recusandae vitae asperiores alias dolores temporibus ipsam perferendis id voluptatibus, fugiat sint ad expedita dolorum voluptatum ut deserunt explicabo voluptate nobis iure animi. Hic odio animi labore nostrum itaque nam repudiandae porro. Cumque beatae velit ullam aspernatur aliquam! Nobis, dolores alias?</p>
     <img src="" alt="blood-test"  className='blood-test'/>
     <li>schedule an appointment online</li>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam recusandae vitae asperiores alias dolores temporibus ipsam perferendis id voluptatibus, fugiat sint ad expedita dolorum voluptatum ut deserunt explicabo voluptate nobis iure animi. Hic odio animi labore nostrum itaque nam repudiandae porro. Cumque beatae velit ullam aspernatur aliquam! Nobis, dolores alias?</p>
     <img src="" alt="online-appointment"  className='online-appointment'/>
     <li>video chat with your doctor</li>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam recusandae vitae asperiores alias dolores temporibus ipsam perferendis id voluptatibus, fugiat sint ad expedita dolorum voluptatum ut deserunt explicabo voluptate nobis iure animi. Hic odio animi labore nostrum itaque nam repudiandae porro. Cumque beatae velit ullam aspernatur aliquam! Nobis, dolores alias?</p>
     <img src="" alt="video-chat"  className='video-chat'/>
     </ul>
     </div>

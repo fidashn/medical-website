806ef71c2fcbf9bbe68c2fcd009a67976f8ec00e
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-15 16:49:27 +0300

    restyled the menus

--- src/Components/OurStaff.js
+++ src/Components/OurStaff.js
@@ -3,7 +3,7 @@ import React from 'react'
 function OurStaff() {
   return (
     <div className='gallery'>
-    <h2>OurStaff</h2>
+    <h2>Our Staff</h2>
     <div className='image1'><img src="../../public/imgs/doctor1.avif" alt="" /></div>
     <div className='image2'><img src="../../public/imgs/doctor2.avif" alt="" /></div>
     <div className='image3'><img src="../../public/imgs/doctor3.avif" alt="" /></div>
